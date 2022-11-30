122ef0567814d4c6a84e80c8c595481b0dd1487b
merts <merts@MERT>
2022-11-30 21:03:06 +0300

    Add project files.

---
+++ AbstractFactoryPattern/Concreate/AbstractFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactoryPattern
+{
+    public class AbstractFactory : IAbstractFactory
+    {
+        public InterFace CreateClassA()
+        {
+            return new ClassA();
+        }
+
+        public InterFace CreateClassB()
+        {
+            return new ClassB();
+        }
+
+        public InterFace CreateClassC()
+        {
+            return new ClassC();
+        }
+    }
+}
